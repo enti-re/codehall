c575044127e0e87f32ed1e3e87cefb3baa73539b
Nikhil Chandna <nikhil.chandna@zopsmart.com>
2023-10-29 11:41:26 +0530

    implement app header

---
+++ components/AppHeader/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SortDropdown from "../SortDropdown";
+
+const AppHeader = () => {
+
+  return (
+    <div className="flex flex-row gap-4">
+      <SortDropdown />
+    </div>
+  );
+};
+
+export default AppHeader;
